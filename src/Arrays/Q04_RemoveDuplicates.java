b8b67f0a07fac560fcef8ec6168d8bf3fb5647f0
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-04-02 00:00:52 +0530

    Remove duplicates from array

---
+++ src/Arrays/Q04_RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Q04_RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0 || nums.length == 1) return 0;
+        int j=1;
+        for(int i=1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+    }
+}
