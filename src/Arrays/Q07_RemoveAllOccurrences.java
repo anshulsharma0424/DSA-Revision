554485013291eae3b96be047c9ec6484bb5db3b7
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-04-02 00:14:55 +0530

    Remove All Occurrences of an Element in an Array

---
+++ src/Arrays/Q07_RemoveAllOccurrences.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Q07_RemoveAllOccurrences {
+
+    public static int removeElement(int[] nums, int element) {
+        int j=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != element) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 3, 0, 2, 2, 4, 2};
+        System.out.println(removeElement(arr, 2));
+    }
+}
