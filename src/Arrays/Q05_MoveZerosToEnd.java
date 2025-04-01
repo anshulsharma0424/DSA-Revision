e4d2ff0add4b9c948a0df65c992551ffff7d0fa6
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-04-02 00:02:03 +0530

    Move zeros to the end using double and single traversals

---
+++ src/Arrays/Q05_MoveZerosToEnd.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Q05_MoveZerosToEnd {
+
+    public static int[] moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+        for(int i=j; i<nums.length; i++){
+            nums[i]=0;
+        }
+        return nums;
+    }
+
+    public static int[] moveZeroes2(int[] arr) {
+        // Pointer to track the position for next non-zero element
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // If the current element is non-zero
+            if (arr[i] != 0) {
+
+                // Swap the current element with the 0 at index 'j'
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                // Move 'j' pointer to the next position
+                j++;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        System.out.println(Arrays.toString(moveZeroes(arr)));
+    }
+}
