c04a398d7931aa07a3f6668559a368bed030ef25
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-04-02 00:09:19 +0530

    Find the Maximum consecutive ones or zeros

---
+++ src/Arrays/Q06_MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Q06_MaxConsecutiveOnes {
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int current = 0, max = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                current++;
+            } else if (num == 0) {
+                max = Math.max(max, current);
+                current = 0;
+            }
+        }
+        return Math.max(max, current);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
